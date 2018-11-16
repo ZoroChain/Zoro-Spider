bfed511c62a8998784fc19ae944c21a8d37bfb6c
Michael Yao <michaely2008@sina.com>
2018-11-16 09:47:46 +0800

    加入UnhandledException日志

--- Program.cs
+++ Program.cs
@@ -17,8 +17,45 @@ namespace Zoro.Spider
         private static LogLevel logLevel = LogLevel.Info;
         private static object logLock = new object();
 
+        private static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            using (FileStream fs = new FileStream("error.log", FileMode.Create, FileAccess.Write, FileShare.None))
+            using (StreamWriter w = new StreamWriter(fs))
+                if (e.ExceptionObject is Exception ex)
+                {
+                    PrintErrorLogs(w, ex);
+                }
+                else
+                {
+                    w.WriteLine(e.ExceptionObject.GetType());
+                    w.WriteLine(e.ExceptionObject);
+                }
+        }
+
+        private static void PrintErrorLogs(StreamWriter writer, Exception ex)
+        {
+            writer.WriteLine(ex.GetType());
+            writer.WriteLine(ex.Message);
+            writer.WriteLine(ex.StackTrace);
+            if (ex is AggregateException ex2)
+            {
+                foreach (Exception inner in ex2.InnerExceptions)
+                {
+                    writer.WriteLine();
+                    PrintErrorLogs(writer, inner);
+                }
+            }
+            else if (ex.InnerException != null)
+            {
+                writer.WriteLine();
+                PrintErrorLogs(writer, ex.InnerException);
+            }
+        }
+
         static void Main(string[] args)
         {
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+
             ProjectInfo.head();
 
             MysqlConn.conf = Settings.Default.MysqlConfig;
