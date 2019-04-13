491779ad85ed87093e4dace9771c54d231d21598
lizhan <joshua0526@163.com>
2019-04-13 19:14:28 +0800

    update

--- Save/SaveTxScriptMethod.cs
+++ Save/SaveTxScriptMethod.cs
@@ -38,75 +38,32 @@ namespace Zoro.Spider
                 MysqlConn.Delete(DataTableName, where);
             }
             scriptMethods.Clear();
-            cutString(script);
-            for (int i = 0; i < scriptMethods.Count; i++) {
-                List<string> slist = new List<string>();
-                slist.Add(txid);
-                slist.Add(scriptMethods[i].calltype);
-                slist.Add(scriptMethods[i].method);
-                slist.Add(scriptMethods[i].contract);
-                slist.Add(blockHeight.ToString());
-                if (scriptMethods[i].contract.Length >= 40)
-                contractState.SaveAsync(scriptMethods[i].contract);
-                MysqlConn.ExecuteDataInsert(DataTableName, slist);
-            }            
+            Op[] op = Avm2Asm.Trans(script.HexToBytes());
+            for (int i = 0; i < op.Length; i++) {
+                if (op[i].code == OpCode.APPCALL)
+                {
+                    string method = Encoding.Default.GetString(op[i - 1].paramData);
+                    string contract = op[i].paramData.Reverse().ToHexString();
+                    AddMethod(txid, "AppCall", method, contract, blockHeight);
+                }
+                else if (op[i].code == OpCode.SYSCALL) {
+                    string method = Encoding.Default.GetString(op[i - 1].paramData);
+                    string contract = Encoding.Default.GetString(op[i].paramData);
+                    AddMethod(txid, "SysCall", method, contract, blockHeight);
+                }
+            }           
         }
 
-        public void cutString(string sb)
-        {
-            List<string> list = new List<string>();            
-            bool isAppCall = false;
-            bool create = false;
-            bool isFirst = true;
-            if (sb.LastIndexOf("68") > 0 && sb.Substring(sb.LastIndexOf("68")).Length < 50 && sb.Substring(sb.LastIndexOf("68")).Length % 2 == 0)
-            {
-                string ss = sb.Substring(sb.LastIndexOf("68") + 4);
-                ss = Encoding.UTF8.GetString(ss.HexToBytes());
-                if (ss == "Zoro.NativeNEP5.Create")
-                {
-                    create = true;
-                    scriptMethods.Add(new ScriptMethod("SysCall", "NativeNEP5.Create", ss));
-                }
-                else if (ss == "Zoro.Contract.Create")
-                {
-                    create = true;
-                    scriptMethods.Add(new ScriptMethod("SysCall", "Contract.Create", ss));
-                }
-            }
-            if (!create)
-            for (var i = 0; i < sb.Length / 2; i++)
-            {
-                var length = System.Convert.ToInt32(sb.Substring(i * 2, 2), 16);
-                if (!isFirst) {
-                    if (length == 103)
-                    {
-                        scriptMethods.Add(new ScriptMethod("AppCall", Encoding.UTF8.GetString(list[list.Count - 1].ToString().HexToBytes()), sb.Substring(i * 2 + 2, 20 * 2).HexToBytes().Reverse().ToHexString()));
-                        isAppCall = true;
-                    }
-                    else if (length == 104)
-                    {
-                        int l = System.Convert.ToInt32(sb.Substring((i + 1) * 2, 2), 16);
-                        scriptMethods.Add(new ScriptMethod("SysCall", Encoding.UTF8.GetString(list[list.Count - 1].ToString().HexToBytes()), Encoding.UTF8.GetString(sb.Substring(i * 2 + 4, l * 2).HexToBytes())));
-                        length = l + 1;
-                    }
-                    else if (length < 1 || (length > 74 && length < 80) || (length > 80 && length < 110) ||
-                        (length > 113 && length < 136) || (length > 138 && length < 142) || (length > 142 && length < 157)
-                        || (length > 157 && length < 166) || (length > 166 && length < 171) || (length > 171 && length < 175) || (length > 191 && length < 206)
-                        || (length > 223 && length < 229) || (length > 239 && length < 242)) continue;
-                }
-                isFirst = false;
-                if (isAppCall)
-                {
-                    isAppCall = false;
-                    list.Add(sb.Substring(i * 2 + 2, 20 * 2));
-                    i += 20;
-                }
-                else
-                {
-                    list.Add(sb.Substring(i * 2 + 2, length * 2));
-                    i += length;
-                }
-            }
+        private void AddMethod(string txid, string code, string method, string contract, uint blockHeight) {
+            List<string> slist = new List<string>();
+            slist.Add(txid);
+            slist.Add(code);
+            slist.Add(method);
+            slist.Add(contract);
+            slist.Add(blockHeight.ToString());
+            if (contract.Length >= 40)
+                contractState.SaveAsync(contract);
+            MysqlConn.ExecuteDataInsert(DataTableName, slist);
         }
     }
 
